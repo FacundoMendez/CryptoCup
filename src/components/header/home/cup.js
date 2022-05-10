284b989d6b0c428d71c4691ae15afecda67c45ea
FacuMendez <facundo.mendez@live.com.ar>
2022-05-10 01:31:20 -0300

    .

--- src/components/header/home/cup.js
+++ src/components/header/home/cup.js
@@ -15,12 +15,12 @@ const cup = () => {
         /* sizes */
 
     const sizes = {
-        width: 500,
+        width: 600,
         height: 800
     }
 
     window.addEventListener("resize", () =>{
-        sizes.width = 500
+        sizes.width = 600
         sizes.height = 800
 
         camera.aspect = sizes.width / sizes.height
@@ -40,26 +40,34 @@ const cup = () => {
     })
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+    renderer.physicallyCorrectLights = true
 
 
     /* ------------------------------------------------- */
 
-
+    
      
 
         /* lights */
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-        scene.add(ambientLight)
-
-        const pointLight = new THREE.PointLight(0xffffff, 10)
-        pointLight.position.set(4 , 0 , 0)
+        const direccionalLight = new THREE.DirectionalLight(0xffffff,8)
+        direccionalLight.position.set(0, 0, 4)
+        scene.add(direccionalLight)
+     
+        const pointLight = new THREE.PointLight(0xffffff,35)
+        pointLight.position.set(4 , 0, 2)
         scene.add(pointLight)
 
-        const pointLight2 = new THREE.PointLight(0xffffff, 10)
-        pointLight2.position.set(-4 , 0 , 0)
+        const pointLight2 = new THREE.PointLight(0xffffff, 20)
+        pointLight2.position.set(-2 , 0, 1)
         scene.add(pointLight2)
 
+        const pointLight3 = new THREE.PointLight(0xffffff, 30)
+        pointLight3.position.set(4 , 4 , 0)
+        scene.add(pointLight3)
+
+        const pointLight4 = new THREE.PointLight(0xffffff, 30)
+        pointLight4.position.set(-3 , 8, -2)
+        scene.add(pointLight4)
 
         /* camera */
         
