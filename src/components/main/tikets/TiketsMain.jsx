f41f28325487ed3f924bb74e72d2e9173a227e65
FacuMendez <facundo.mendez@live.com.ar>
2022-05-13 16:23:18 -0300

    .

--- src/components/main/tikets/TiketsMain.jsx
+++ src/components/main/tikets/TiketsMain.jsx
@@ -11,9 +11,8 @@ const TiketsMain = () => {
 
   return (
     <div className="containerTikets">
-        <TitlesContainers title="TIKETS" />
-
         <div className="textTikets">
+            <TitlesContainers title="TIKETS" />
             <p>By buying a ticket you acces the Tournamet as a player, and you will be able to use all the functions of the Dapp and place bets on all the matches of the World Cup</p>
         </div>
     </div>
