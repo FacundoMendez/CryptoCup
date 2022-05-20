e36231d3a8914f4bee3c9de1939aa1612036e601
facuMendez <facundo.mendez@live.com.ar>
2022-05-20 13:05:18 -0300

    .

--- src/components/tiketBuy/homeTikets/TiketBuyPage.jsx
+++ src/components/tiketBuy/homeTikets/TiketBuyPage.jsx
@@ -15,44 +15,44 @@ const TiketBuyPage = () => {
       
       <div className="flexTickets">
 
-        <div className="ticketsSale">
-          <div className="boxTickets">
-              <img src={tiketBasico} alt="Ticket Bassic" />
+          <div className="ticketsSale">
+              <div className="boxTickets">
+                  <img src={tiketBasico} alt="Ticket Bassic" />
 
-              <div className="wrapperBotton-Tickets">
-                  <div className="btnButton-Tickets">
-                      <p>BUY</p>
-                      <span className="BorderTopBottom-Tickets "></span>
-                      <span className="BorderLeftRight-Tickets "></span>
+                  <div className="wrapperBotton-Tickets">
+                      <div className="btnButton-Tickets">
+                          <p>BUY</p>
+                          <span className="BorderTopBottom-Tickets "></span>
+                          <span className="BorderLeftRight-Tickets "></span>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="boxTickets">
+                  <img src={tiketBoost} alt="Ticket Ladder" />
+
+                  <div className="wrapperBotton-Tickets">
+                      <div className="btnButton-Tickets">
+                          <p>BUY</p>
+                          <span className="BorderTopBottom-Tickets "></span>
+                          <span className="BorderLeftRight-Tickets "></span>
+                      </div>
                   </div>
               </div>
           </div>
 
-          <div className="boxTickets">
-              <img src={tiketBoost} alt="Ticket Ladder" />
-
-              <div className="wrapperBotton-Tickets">
-                  <div className="btnButton-Tickets">
-                      <p>BUY</p>
-                      <span className="BorderTopBottom-Tickets "></span>
-                      <span className="BorderLeftRight-Tickets "></span>
-                  </div>
-              </div>
+          <div className="text-tickets">
+            <div className="titleTicket">
+              <h2>BOOST LADDER <br />
+                TOURNAMENT
+              </h2>
+            </div>
+            <div className="textoTicket">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam tempora praesentium eius esse dignissimos minima at architecto ipsam dicta voluptas amet, odio non cumque pariatur repellat velit repellendus. Culpa, labore.
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className="text-tickets">
-          <div className="titleTicket">
-            <h2>BOOST LADDER <br />
-              TOURNAMENT
-            </h2>
-          </div>
-          <div className="textoTicket">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam tempora praesentium eius esse dignissimos minima at architecto ipsam dicta voluptas amet, odio non cumque pariatur repellat velit repellendus. Culpa, labore.
-            </p>
-          </div>
-
-        </div>
+          
       </div>
     </div>
   )
