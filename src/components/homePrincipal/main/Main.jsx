97604c0522a2dd33c3d90d15f9d1459dc8274019
facuMendez <facundo.mendez@live.com.ar>
2022-05-24 10:47:07 -0300

    .

--- src/components/homePrincipal/main/Main.jsx
+++ src/components/homePrincipal/main/Main.jsx
@@ -1,17 +1,12 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import TokenMain from './token/TokenMain'
 import TournamentMain from './tournament/TournamentMain'
 import TiketsMain from './tikets/TiketsMain'
-/* import ball from './ball' */
 import Roadmap from './roadmap/Roadmap'
 import VideoPubli from './videoPubli/VideoPubli'
 
 
 const Main = () => {
-
-    useEffect(() =>{
-        /* ball() */
-    },[])
 
   return (
     <main>
