e23355c2482ef361a4ae12cf3e124c8ef3409c59
facuMendez <facundo.mendez@live.com.ar>
2022-06-07 14:56:27 -0300

    .

--- src/components/tiketBuy/homeTikets/tokensSelect/tokenBasic/checkTokenBasic.js
+++ src/components/tiketBuy/homeTikets/tokensSelect/tokenBasic/checkTokenBasic.js
@@ -1,7 +1,9 @@
 const checkTokenBasic = () => {
     const checkButton = document.querySelector(".btn-ticketSale ")
     const radioBox = document.querySelector(".radioToken")
-
+/*     const radioBoxActive = document.querySelector(".radioToken-active")
+    let indicador = document.getElementsByClassName("control_indicator") */
+    
     checkButton.addEventListener("click", function(){
         radioBox.classList.toggle("radioToken-active");
     })
@@ -10,6 +12,11 @@ const checkTokenBasic = () => {
         radioBox.classList.toggle("radioToken");
     })
 
+/*     for (var i = 0; i < indicador.length; i++) {
+        indicador[i].addEventListener("click", function(){
+            radioBox.classList.toggle("radioToken");
+        })
+    } */
  
 
 }
