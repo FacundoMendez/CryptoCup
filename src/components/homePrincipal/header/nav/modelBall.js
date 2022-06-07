b943f2c9649e2597a107a6ab3e106f6e6c09c743
Facu <facundo.mendez@live.com.ar>
2022-06-07 19:27:55 -0300

    .

--- src/components/homePrincipal/header/nav/modelBall.js
+++ src/components/homePrincipal/header/nav/modelBall.js
@@ -17,13 +17,13 @@ const navBallModel = () => {
             /* sizes */
 
         const sizes = {
-            width: 200,
-            height: 200
+            width: 190,
+            height: 190
         }
 
         window.addEventListener("resize", () =>{
-            sizes.width = 200
-            sizes.height = 200
+            sizes.width = 190
+            sizes.height = 190
 
             camera.aspect = sizes.width / sizes.height
             camera.updateProjectionMatrix()
