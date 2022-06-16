2fb8189d3f874066f1948e46217fbb2e422cbfc5
Facu <facundo.mendez@live.com.ar>
2022-06-16 12:16:33 -0300

    .

--- src/components/homePrincipal/main/nfts/cardsFuncional.js
+++ src/components/homePrincipal/main/nfts/cardsFuncional.js
@@ -1,32 +1,39 @@
 import VanillaTilt from "vanilla-tilt";
 
 const cardsFuncional = () => {
-    VanillaTilt.init(document.querySelectorAll(".tiketImg-basic"), {
-        max: 25,
-        speed: 1000,
-        "max-glare":1,
-    });
+
+    function isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
+    if (!isMobile()) {
+        VanillaTilt.init(document.querySelectorAll(".tiketImg-basic"), {
+            max: 25,
+            speed: 1000,
+            "max-glare":1,
+        });
+        
+        VanillaTilt.init(document.querySelectorAll(".tiketImg-boost"), {
+            max: 25,
+            speed: 1000,
+            "max-glare":1,
+        });
+
+
+        VanillaTilt.init(document.querySelectorAll(".container_card"), {
+            max: 25,
+            speed: 1000,
+            glare:true,
+            "max-glare":1,
+        });
+
+        VanillaTilt.init(document.querySelectorAll(".textRoadamp"), {
+            max: 25,
+            speed: 1000,
+            "max-glare":1,
+        });
+    }
     
-    VanillaTilt.init(document.querySelectorAll(".tiketImg-boost"), {
-        max: 25,
-        speed: 1000,
-        "max-glare":1,
-    });
-
-
-    VanillaTilt.init(document.querySelectorAll(".container_card"), {
-        max: 25,
-        speed: 1000,
-        glare:true,
-        "max-glare":1,
-    });
-
-    VanillaTilt.init(document.querySelectorAll(".textRoadamp"), {
-        max: 25,
-        speed: 1000,
-        "max-glare":1,
-    });
-
 }
 
 
