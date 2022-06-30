d8f41fddd7da45757772df57d3d4dea67a920d9a
Facu <facundo.mendez@live.com.ar>
2022-06-29 21:55:43 -0300

    .

--- src/components/error404/Error404.jsx
+++ src/components/error404/Error404.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import "./error404.css"
 import logo from "../homePrincipal/main/src/Logo.png"
+import particulas from "../tiketBuy/homeTikets/src/particle1P.mp4"
 
 const Error404 = () => {
   return (
     <div className='containerError404'>
+      <video className='particulas404' src={particulas} autoPlay loop muted ></video>
+        <div className="imgeerror"></div>
         <div className="glitch-wrapper">
             <div className="glitch-text">
-                ERROR 404: Not found
+                <h2> ERROR 404 <br /> <span>NOT FOUND</span>  </h2> 
             </div>
-
         </div>
 
-        <a className='enlace404' href="/"><button className='buttonError404' type="button">Homepage</button></a>
+        <a className='enlace404' href="/">
+          <div className="wrapperBotton-error404">
+            <div className="btn-error404">
+              <p>HOMEPAGE</p>
+              <span className="BorderTopBottom-error404 "></span>
+              <span className="BorderLeftRight-error404 "></span>
+            </div>
+          </div>
+        </a>
 
         <img className='logoError404' src={logo} alt="Logo" />
     </div>
