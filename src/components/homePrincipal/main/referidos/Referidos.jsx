ab6b546e3a78a135584ce9c8947b2cbe530e1656
facuMendez <facundo.mendez@live.com.ar>
2022-07-07 13:38:52 -0300

    .

--- src/components/homePrincipal/main/referidos/Referidos.jsx
+++ src/components/homePrincipal/main/referidos/Referidos.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import "./referidos.css"
 import backReferidos from "./src/ref4.jpg"
+import linesBack from "./src/lines.jpg"
 
 const Referidos = () => {
   return (
     <div className='containerReferidos'>
         <img className="backReferidos" src={backReferidos} alt="background referidos" />
-        <div className="cortinaTop"></div>
+        <div className="cortinaTop">
+          <img className='linesBackTop' src={linesBack} alt="linesBack" />
+        </div>
         <div className="textsReferidos">
             <h2>refer to earn</h2>
             <p>The referral system allowed you to bring people and get a huge discount and the oportunity to you to get big rewards <br /> <br /> <span> For every people who bougth a ticket with your referral code, <br /> you get <span className='spanColor'>$1 USDT</span> , and they get a discount of <span className='spanColor'>$4 USDT</span>!</span></p>
         </div>
-        <div className="cortinaBottom"></div>
+        <div className="cortinaBottom">
+          <img className='linesBackBottom' src={linesBack} alt="linesBack" />
+        </div>
     </div>
   )
 }
