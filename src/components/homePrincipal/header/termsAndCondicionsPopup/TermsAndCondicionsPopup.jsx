74a01b6229abc60dcd0ba7995e8603b2c63f3946
Facu <facundo.mendez@live.com.ar>
2022-07-08 17:33:01 -0300

    .

--- src/components/homePrincipal/header/termsAndCondicionsPopup/TermsAndCondicionsPopup.jsx
+++ src/components/homePrincipal/header/termsAndCondicionsPopup/TermsAndCondicionsPopup.jsx
@@ -15,14 +15,6 @@ const TermsAndCondicionsPopup = () => {
         <div className="termsAndCondicionsPopup_popup">
             <p>By accepting you are confirming that you have read, understood and agree to Crypto Cup's <a href="/TermsAndConditions">Terms and Conditions</a> </p>
             <div className="termsAndCondicionsPopup_buttons">
-                <button className="termsAndCondicionsPopup_buttons-decline" onClick={() => {
-                    termsFuncionalDecline()
-
-                    context.setCheckTerminosYCondiciones(false);
-                    sessionStorage.setItem("terms and Conditions", "Decline")
-
-                    }}>Decline</button>
-
                 <button className="termsAndCondicionsPopup_buttons-acept" onClick={() => {
 
                     context.setCheckTerminosYCondiciones(true);
