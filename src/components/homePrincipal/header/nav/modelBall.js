741a292988b64acae81cc56d046f00d52c7d5de7
FacuMendez <facundo.mendez@live.com.ar>
2022-07-17 12:36:06 -0300

    .

--- src/components/homePrincipal/header/nav/modelBall.js
+++ src/components/homePrincipal/header/nav/modelBall.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import ballModel from "./src/models/Pelota2.gltf"
+import ballModel from "./src/models/PelotaGod.glb"
 
 
 const navBallModel = () => {
