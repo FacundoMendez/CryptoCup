7e7bc03963af1298e9f55628d3dff291ab87f062
facuMendez <facundo.mendez@live.com.ar>
2022-07-19 14:39:11 -0300

    .

--- src/components/homePrincipal/header/home/Home.jsx
+++ src/components/homePrincipal/header/home/Home.jsx
@@ -1,7 +1,8 @@
-import React, {lazy} from 'react'
+import React, {lazy, useEffect, useState} from 'react'
 import "./home.css"
 import backCopaPrincipal from "./src/videos/backCopa.mp4"
 import soonImg from "./src/comingSoon.png"
+import backImagenCopa from "./src/backCopaImg.jpg"
 
 const Button = lazy(() => import ("../../buttons/Button"))
 const Social = lazy(() => import ('./social/Social'))
@@ -10,11 +11,31 @@ const Timer = lazy(() => import ('./timer/Timer'))
 
 const Home = () => {
 
+  const [esMovil , setEsMovil] = useState(false)
+
+  function isMobile(){
+    return (
+        (navigator.userAgent.match(/Android/i)) ||
+        (navigator.userAgent.match(/webOS/i)) ||
+        (navigator.userAgent.match(/iPhone/i)) ||
+        (navigator.userAgent.match(/iPod/i)) ||
+        (navigator.userAgent.match(/BlackBerry/i))
+    );
+}
+
+  useEffect(() => {
+    if(isMobile()){
+      setEsMovil(true)
+    } else {
+      setEsMovil(false)
+    } 
+  },[])
+ 
   return (
     <div id='home' className="containerHome">
 
       <div className="containerPrincipalHome">
-{        <video className='backPrincipal' src={backCopaPrincipal} autoPlay loop muted  type="video/mp4"></video> }
+   {  !esMovil  ?   <video className='backPrincipal' src={backCopaPrincipal} autoPlay loop muted  type="video/mp4"></video> :  <img className='backPrincipal' src={backImagenCopa} alt="backPrincialMovil" /> }
         <div className='textPrincipalHome'>
           <div className="tite-home">
               <h1 className='titlePrincipal'>CRYPTO CUP <br />
@@ -23,7 +44,8 @@ const Home = () => {
               
               <div className="buttonsHome">
                 <a href="./tickets">
-                  <Button title="BUY TICKETS"/>
+                  {!esMovil ? <Button title="BUY TICKETS"/> : <Button title="TICKETS"/>}
+                  
                 </a>
             {/* <a href="./presale"> */}
                 <div className=" wrapperBottonPresale">
@@ -40,9 +62,9 @@ const Home = () => {
             <Timer/>
           </div>
         </div>
-        <Social />
       </div>    
     
+      <Social />
 
     </div>
 
