718ab29b0a84af78c9014c1e8dea83d7ab3b412f
Facu <facundo.mendez@live.com.ar>
2022-07-23 18:03:11 -0300

    .

--- src/components/homePrincipal/main/referidos/Referidos.jsx
+++ src/components/homePrincipal/main/referidos/Referidos.jsx
@@ -12,7 +12,7 @@ const Referidos = () => {
         </div>
         <div className="textsReferidos">
             <h2>refer to earn</h2>
-            <p>The referral system allowed you to bring people and get a huge discount and the oportunity to get big rewards <br /> <br /> <span> For every people who bougth a ticket with your referral code, <br /> you get <span className='spanColor'>$1 USDT</span> , and they get a discount of <span className='spanColor'>$4 USDT</span></span></p>
+            <p>The referral system allowed you to bring people and get a huge discount and the oportunity to get big rewards <br /> <br /> <span> For every people who bougth a ticket with your referral code, <br /> you get <span className='spanColor'>$2 USDT</span> , and they get a discount of <span className='spanColor'>$4 USDT</span></span></p>
         </div>
         <div className="cortinaBottom">
           <img className='linesBackBottom' src={linesBack} alt="linesBack" />
