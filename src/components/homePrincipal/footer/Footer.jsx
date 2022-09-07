af13a769e1c5f9ac5f74267bdd45af0a0c5ccb70
FacuMendez <facundo.mendez@live.com.ar>
2022-09-07 18:23:29 -0300

    reconstruyendo footer

--- src/components/homePrincipal/footer/Footer.jsx
+++ src/components/homePrincipal/footer/Footer.jsx
@@ -5,14 +5,64 @@ import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { faInstagram } from "@fortawesome/free-brands-svg-icons"
 import { faTelegram } from "@fortawesome/free-brands-svg-icons"
 import { faDiscord } from "@fortawesome/free-brands-svg-icons"
-import { faYoutube } from "@fortawesome/free-brands-svg-icons"
 import {FormattedMessage} from 'react-intl';
 
 const Footer = () => {
   return (
     <div className="footer">
-        <img className='backFooter' src= "https://res.cloudinary.com/dvrxw8fbg/image/upload/v1660661016/CryptocupQatar/footer/Footer3_hxey1j.webp"/* {backFooter} */ alt="Background Footer" />
         <div className="blurFooter"></div>
+        <div className="circuloMovimiento">
+            <div className="socialFooter">
+                <a href="https://twitter.com/CryptoCupQatar" target="_blank" rel="noopener noreferrer" aria-label='twitter red social'><i className="twitterSocialFooter"> <FontAwesomeIcon icon={faTwitter}  />  </i> Twitter  </a>
+                <a href="https://www.instagram.com/cryptocupqatar/" target="_blank" rel="noopener noreferrer" aria-label='instagram red social'><i className="instagramSocialFooter"> <FontAwesomeIcon icon={faInstagram} /> </i> Instagram  </a>
+                <a href="https://t.me/cryptocup2022" target="_blank" rel="noopener noreferrer" aria-label='telegram red social'><i className="telegramSocialFooter"> <FontAwesomeIcon icon={faTelegram} /> </i> Telegram  </a>
+                <a href="https://discord.gg/ckFmn9xXhw" target="_blank" rel="noopener noreferrer" aria-label='discord red social'><i className="discordSocialFooter"> <FontAwesomeIcon icon={faDiscord} /> </i> Discord </a>
+            </div> 
+        </div>
+
+        <div className="boxBottomFotter">
+            <div className="terms">
+                    <div className='listTerms'>
+                        <ul>
+                            <li>
+                                <a href="TermsAndConditions" >
+                                    <FormattedMessage
+                                        id="footer-tyc"
+                                        defaultMessage="• Terms and Conditions"
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                        <br />
+                    </div>
+                </div>
+
+                <div className="emailContac">
+                    <p className='titleContact' >
+                        <FormattedMessage
+                            id="footer-contact"
+                            defaultMessage="Contact:"
+                        />
+                    </p>
+                    <a href="mailto:cryptocup2022@gmail.com">
+                        <p className='email' >
+                            <FormattedMessage
+                                id="footer-email"
+                                defaultMessage="Cryptocup2022@gmail.com "
+                            />
+                        </p>
+                    </a> 
+                </div>
+
+            <p className='termsAndCond'>
+                    Copyright © 2022. All Rights Reserved by CryptoCupQatar
+            </p> 
+        </div>
+
+
+
+{/*         <img className='backFooter' src= "https://res.cloudinary.com/dvrxw8fbg/image/upload/v1660661016/CryptocupQatar/footer/Footer3_hxey1j.webp" alt="Background Footer" />
+      
 
         <div className="boxFooter">
             <div className="logoFooter">
@@ -70,7 +120,7 @@ const Footer = () => {
 
         <p className='termsAndCond'>
                 Copyright © 2022. All Rights Reserved by CryptoCupQatar
-        </p>
+        </p> */}
     </div>
   )
 }
