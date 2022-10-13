9881cc2efc6bc255394709786b3f3c1c466aca52
FacuMendez <facundo.mendez@live.com.ar>
2022-10-13 00:38:51 -0300

    .

--- src/components/homePrincipal/main/token/TokenMain.jsx
+++ src/components/homePrincipal/main/token/TokenMain.jsx
@@ -155,7 +155,7 @@ const TokenMain = () => {
 
         <div className="grafico">
             <h2>Tokenomics</h2>
-            <img src="https://res.cloudinary.com/dvrxw8fbg/image/upload/c_scale,h_1080,q_auto:best,w_1920/v1665112547/CryptocupQatar/token/grafico_qe82r9.png" alt="grafico" />
+            <img src="https://res.cloudinary.com/dvrxw8fbg/image/upload/c_scale,h_1080,q_auto:best,w_1920/v1665631492/CryptocupQatar/token/PieChartDEFINITIVO_o2gxlb.png" alt="grafico" />
         </div>
 
     </div>
